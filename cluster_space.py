0910066ee3886336c38f9a03e83deeb4fcdeab47
thien.thai <tthien@apcs.vn>
2017-01-20 10:24:36 +0700

    update from main project (Vinh)

--- cluster_space.py
+++ cluster_space.py
@@ -1,5 +1,21 @@
 from face_pic import FacePic
+from face_cluster import FaceCluster
 from collections import *
+import itertools
+from os import listdir
+import numpy as np
+from os.path import isfile
+import dill
+import pickle
+from Encoder import encoder
+from munkres import Munkres
+
+E = encoder()
+RECOGNISE_THRESHOLD = 0.9
+MERGE_DISTANCE_THRESHOLD = 2.0
+INCREMENTAL_TRAIN_THRESHOLD = 20
+TRAIN_THRESHOLD = 40
+SMALLEST_SIZE_NAME_THRESHOLD = 8
 
 
 def _find_key(my_dict, value):
@@ -18,11 +34,16 @@ def _find_key(my_dict, value):
     return key
 
 class Clusterspace:
-    def __init__(self, clusters):
+    def __init__(self, data_dir, clusters = None, mac = None):
+        self.mac = mac
+        self.data_dir = data_dir
         self.clusters_space = []
         self.distances = defaultdict()
-        self.clusters_space += clusters
-        self.calculate_distance()
+        self.frams = set()
+        self.host = None
+        if clusters:
+            self.clusters_space += clusters
+            self.calculate_distance()
 
     def add_clusters(self, clusters):
         """
@@ -32,10 +53,180 @@ class Clusterspace:
         """
         for cl in clusters:
             if len(cl) != 0:
-                self.clusters_space += clusters
+                self.clusters_space.append(cl)
 
         self.calculate_distance()
 
+    def read_fram(self,f_path):
+        framID = f_path.split("/")[-1]
+        clusters = []
+        for cluster in listdir(f_path):
+            face_cluster = set()
+            for pic in listdir(f_path + "/" + cluster):
+                path = f_path + "/" + cluster + "/" + pic
+                face_cluster.add(FacePic(E.get_rep_preprocessed(path),framID,path))
+            clusters.append(FaceCluster(face_cluster))
+        return clusters
+
+    def update_fram(self):
+        cur_frams = set()
+        for frame in self.data_dir:
+            cur_frams.add(frame)
+        return cur_frams
+
+    def renew(self):
+        self.clusters_space = []
+        self.distances = defaultdict()
+        self.frams = set()
+
+    def save_model(self):
+        d = defaultdict()
+        d["clusters_space"] = self.clusters_space
+        d["data_dir"] = self.data_dir
+        d["distances"] = self.distances
+        d["frams"] = self.frams
+        pickle.dump(d, open("trained_space.p", "wb"), protocol=2)
+
+    def load_model(self):
+        space = pickle.load(open("trained_space.p", "rb"))
+        self.clusters_space = space["clusters_space"]
+        self.data_dir = space["data_dir"]
+        self.distances = space["distances"]
+        self.frams = space["frams"]
+
+    def incremental_train(self):
+        print "-----------------------------INCREMENTAL TRAINING----------------------------------"
+        new_clusters = []
+        newframs = self.update_fram() - self.frams
+        if len(newframs) == 0:
+            return
+        for fram in newframs:
+            f_path = self.data_dir + "/" + fram
+            clusters = self.read_fram(f_path)
+            new_clusters.extend(clusters)
+            self.frams.add(fram)
+        self.add_clusters(new_clusters)
+        self.merge_closest(MERGE_DISTANCE_THRESHOLD)
+        self.name()
+        self.save_model()
+
+    def find_host_cluster(self):
+        pass
+
+    # def train(self):
+    #     print "#fram = " + str(len(self.update_fram()))
+    #     if isfile("trained_space.p") and len(self.clusters_space) == 0:
+    #         self.load_model()
+    #
+    #     newframs = self.update_fram() - self.frams
+    #     if len(newframs) > 0 and len(self.update_fram())%TRAIN_THRESHOLD == 0:
+    #         print "-----------------------------TRAINING----------------------------------"
+    #         self.renew()
+    #
+    #         data = defaultdict(lambda :defaultdict(list))
+    #         path2rep = defaultdict(lambda :0)
+    #         for frame in listdir(self.data_dir):
+    #             for cluster in listdir(self.data_dir + "/" + frame):
+    #                 for pic in listdir(self.data_dir + "/" + frame + "/" + cluster):
+    #                     path = self.data_dir + "/" + frame + "/" + cluster + "/" + pic
+    #                     data[frame][cluster].append(path)
+    #                     path2rep[path] = E.get_rep_preprocessed(path)
+    #
+    #         clusters = []
+    #         for frame in data.keys():
+    #             for cluster in data[frame].keys():
+    #                 FacePics = set()
+    #                 for path in data[frame][cluster]:
+    #                     FacePics.add(FacePic(path2rep[path], frame, path))
+    #                 clusters.append(FaceCluster(FacePics))
+    #             self.frams.add(frame)
+    #         self.add_clusters(clusters)
+    #         self.merge_closest(MERGE_DISTANCE_THRESHOLD)
+    #         self.name()
+    #         self.save_model()
+    #
+    #     elif len(self.update_fram()) > TRAIN_THRESHOLD \
+    #             and len(newframs)%INCREMENTAL_TRAIN_THRESHOLD == 0  \
+    #             and len(newframs)%TRAIN_THRESHOLD != 0:
+    #             self.incremental_train()
+    #     else:
+    #         return
+
+    def match(self, f_path):
+        if len(self.update_fram()) <= TRAIN_THRESHOLD:
+            return None
+        f_path = self.data_dir + "/" + f_path.split("/")[-1]
+        clusters = self.read_fram(f_path)
+        if len(clusters) == 0:
+            return None
+        _, working_cluster_idx = self.getWorkingCluster()
+        dist = np.zeros((len(clusters), len(working_cluster_idx)))
+
+        working_cluster_idx2idx = defaultdict()
+        idx2working_cluster_idx = defaultdict(lambda: -1)
+
+        count = 0
+        for cluster_idx in working_cluster_idx:
+            working_cluster_idx2idx[cluster_idx] = count
+            idx2working_cluster_idx[count] = cluster_idx
+            count += 1
+
+        for acluster in working_cluster_idx:
+            for cluster in range(len(clusters)):
+                dist[cluster][working_cluster_idx2idx[acluster]] \
+                    = (self.clusters_space[acluster]).distance(clusters[cluster])
+
+        for i in range(dist.shape[0]):
+            if np.all(dist[i] > RECOGNISE_THRESHOLD):
+                for j in range(dist.shape[1]):
+                    dist[i][j] = 987654321
+
+        # for i in range(len(clusters)):
+        #     a = clusters[i]
+        #     a.show_faces()
+
+        m = Munkres()
+        dist = pad_to_square(dist, pad_value=0)
+        tmp = dist.tolist()
+        indexes = m.compute(tmp)
+
+        ret = defaultdict()
+        print "-------------------------------MATCH-----------------------------------------"
+        for row, column in indexes:
+            if tmp[row][column] == 987654321:
+                idx2working_cluster_idx[row] = -1
+            if tmp[row][column] != 0:
+                ret[row] = self.clusters_space[idx2working_cluster_idx[column]].name
+                print "\t" + str(row) + " : " + \
+                      self.clusters_space[idx2working_cluster_idx[column]].name \
+                      + " (dist: " + str(tmp[row][column]) + ")"
+
+        # for i in ret.keys():
+        #     acluster = clusters[i]
+        #     cluster = self.clusters_space[ret[i]]
+        #     acluster.show_faces()
+        #     cluster.show_faces()
+
+        return ret
+
+    def is_matchable(self):
+        pass
+
+    def show__working_cluster(self):
+        _, idx = self.getWorkingCluster()
+        for i in idx:
+            print "Cluster_" + str(i)
+            self.clusters_space[i].show_faces()
+
+    def show_working_cluster_and_distance(self):
+        _, idx = self.getWorkingCluster()
+        for (a, b) in itertools.combinations(idx, 2):
+            dist = self.clusters_space[a].distance(self.clusters_space[b])
+            print "Dist(" + str(a) + " , " + str(b) + ") = " + str(dist)
+            print "Faces in " + str(a)
+            self.clusters_space[a].show_faces()
+            print "Faces in " + str(b)
+            self.clusters_space[b].show_faces()
 
     def calculate_distance(self, force_recalculate = False):
         """
@@ -43,7 +234,6 @@ class Clusterspace:
         :param force_recalculate: recalculate all exist valid cluster
         :return:
         """
-
         # idx_a < idx_b
         for idx_a, cluster_a in enumerate(self.clusters_space):
             for idx_b, cluster_b in enumerate(self.clusters_space):
@@ -62,7 +252,7 @@ class Clusterspace:
                 self.distances[idx_a, idx_b] = cluster_a.distance(cluster_b)
 
 
-    def merge_closest(self, threshold):
+    def merge_closest(self, threshold = MERGE_DISTANCE_THRESHOLD):
         """
         Merge pair of closest cluster
         until closest vector distance > threshold
@@ -79,6 +269,8 @@ class Clusterspace:
             new_cluster, child_cluster, a_child_cluster = self.clusters_space[idx_a].merge(self.clusters_space[idx_b], True)
             self.add_clusters([new_cluster, child_cluster, a_child_cluster])
 
+            if len(self.distances.values()) == 0:
+                break
             ## redo the calculation
             self.calculate_distance()
             min_dist = min(self.distances.values())
@@ -90,8 +282,21 @@ class Clusterspace:
         :return: a list of working cluster
         """
         working_cluster = []
+        idx = []
+        count = 0
         for cluster in self.clusters_space:
             if cluster.working == True:
                 working_cluster.append(cluster)
+                idx.append(count)
+            count += 1
 
-        return working_cluster
+        return working_cluster, idx
+
+    def name(self):
+        print "------------------------------------NAMING-------------------------------------"
+        a, _ = self.getWorkingCluster()
+        for cluster in a:
+            if cluster.name == "no_name" and len(cluster.facepics)>SMALLEST_SIZE_NAME_THRESHOLD:
+                cluster.show_faces()
+                cluster.make_name()
+
--- face_cluster.py
+++ face_cluster.py
@@ -1,8 +1,12 @@
-import numpy as np
-from math import sqrt
 from collections import defaultdict
+from math import sqrt
+import cv2
+import numpy as np
 from numpy import linalg as LA
+from scipy.spatial.distance import mahalanobis
 MERGE_THRESHOLD = 0.4
+#good at 0.1-0.2
+
 
 class FaceCluster:
     """
@@ -22,19 +26,34 @@ class FaceCluster:
         self.facepics = facepics
         self.facepicsrep = None
         self.frame_ids = set()
-        self.freq_frame_ids = defaultdict() # freq each frame id appear
+        self.freq_frame_ids = defaultdict()  # freq each frame id appear
         self.working = True
+        self.name = "no_name"
         for facepic in facepics:
             self.frame_ids.add(facepic.frame_id)
             if (facepic.frame_id not in self.freq_frame_ids.keys()):
                 self.freq_frame_ids[facepic.frame_id] = 1
             else:
-                self.freq_frame_ids[facepic.frame_id] +=1
+                self.freq_frame_ids[facepic.frame_id] += 1
 
             if (type(self.facepicsrep).__module__ != np.__name__):
                 self.facepicsrep = np.array([facepic.rep])
             else:
                 self.facepicsrep = np.vstack([self.facepicsrep, facepic.rep])
+
+    def make_name(self):
+        name = raw_input('Enter name of this cluster: ')
+        self.name = name
+
+    def show_faces(self):
+        for face_pic in self.facepics:
+            path = face_pic.pic_dir
+            img = cv2.imread(path)
+            if img == None:
+                continue
+            cv2.imshow(path,img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     def merge(self, acluster, split_child):
         """
@@ -55,7 +74,22 @@ class FaceCluster:
         facepics = self.facepics | acluster.facepics
         self.working = False
         acluster.working = False
+
+        if self.name == "no_name" and acluster.name == "no_name":
+            name = "no_name"
+        elif self.name == "no_name" or acluster.name == "no_name":
+            if self.name == "no_name":
+                name = acluster.name
+            elif acluster.name == "no_name":
+                name = self.name
+            elif len(self.facepics) > len(acluster.facepics) and self.name != "no_name":
+                name = self.name
+            else:
+                name = acluster.name
+
+
         new_cluster = FaceCluster(facepics)
+        new_cluster.name = name
 
         if split_child:
             return new_cluster, child_cluster, a_child_cluster
@@ -79,12 +113,8 @@ class FaceCluster:
             self.facepics.remove(face)
 
         child_cluster = FaceCluster(splitface)
-        self.__init__(self.facepics) # reinit the facepics
+        self.__init__(self.facepics)  # reinit the facepics
         return child_cluster
-
-
-
-
 
     def __len__(self):
         """
@@ -100,15 +130,15 @@ class FaceCluster:
         :return:
          True or False
         """
-        #collisions_frames = [k for k in self.freq_frame_ids.keys() if k in acluster.freq_frame_ids.keys()]
+        # collisions_frames = [k for k in self.freq_frame_ids.keys() if k in acluster.freq_frame_ids.keys()]
         collisions_frames = self.frame_ids & acluster.frame_ids
-        if len(self.facepics)==0:
-            self.working = False # empty cluster not working
+        if len(self.facepics) == 0:
+            self.working = False  # empty cluster not working
         if len(acluster.facepics) == 0:
             acluster.working = False
 
         if (self.working == False) or (acluster.working == False):
-            return False # not mergable
+            return False  # not mergable
 
         self.ncollision = 0
         ancollision = 0
@@ -116,10 +146,10 @@ class FaceCluster:
             print 'break'
         for collision_id in collisions_frames:
             self.ncollision += self.freq_frame_ids[collision_id]
-            ancollision +=acluster.freq_frame_ids[collision_id]
+            ancollision += acluster.freq_frame_ids[collision_id]
         a = float(self.ncollision) / len(self.facepics)
         b = float(ancollision) / len(acluster.facepics)
-        c = max(a,b)
+        c = max(a, b)
         if c < MERGE_THRESHOLD:
             return True
         else:
@@ -140,172 +170,22 @@ class FaceCluster:
         :return:
             distance of 2 cluster (float)
         """
-        coef = sqrt(float(2*len(self)*len(acluster))/(len(self)+len(acluster)))
-        tmp = LA.norm(self.mean()-acluster.mean())
-        d =  coef * tmp
+        coef = sqrt(float(2 * len(self) * len(acluster)) / (len(self) + len(acluster)))
+        tmp = LA.norm(self.mean() - acluster.mean())
+        d = coef * tmp
         return d
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-302
+    def mahalanobis_distance(self, acluster):
+        sumdist = 0
+        acluster_size = len(acluster.facepicsrep)
+        clus = np.vstack(self.facepicsrep)
+        clus = np.transpose(clus)
+        cov = np.cov(clus)
+        if np.linalg.cond(cov) > np.finfo(cov.dtype).eps:
+            inv_cov = np.linalg.pinv(cov)
+        else:
+            inv_cov = np.linalg.inv(cov)
+        mean = self.mean()
+        for face_rep in acluster.facepicsrep:
+            sumdist += mahalanobis(face_rep, mean, inv_cov)
+        return sumdist/acluster_size
